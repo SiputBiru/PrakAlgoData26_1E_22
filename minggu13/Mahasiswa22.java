aa35d780f001a561ffb67652e3792cf2ee1f9d20
SiputBiru <hillsforrest03@gmail.com>
2026-06-01 08:25:06 +0700

    feat: add Mahasiswa and Node classes for Tree

---
+++ minggu13/Mahasiswa22.java
@@ -0,0 +1,17 @@
+public class Mahasiswa22 {
+    String nim, nama, kelas;
+    double ipk;
+
+    public Mahasiswa22() {}
+
+    public Mahasiswa22(String nim, String nama, String kelas, double ipk) {
+        this.nim = nim;
+        this.nama = nama;
+        this.kelas = kelas;
+        this.ipk = ipk;
+    }
+
+    public void tampilInformasi() {
+        System.out.println("NIM: " + nim + " Nama: " + nama + " Kelas: " + kelas + " IPK: " + ipk);
+    }
+}
---
+++ minggu13/Node22.java
@@ -0,0 +1,12 @@
+public class Node22 {
+    Mahasiswa22 mahasiswa;
+    Node22 left, right;
+
+    public Node22() {}
+
+    public Node22(Node22 left, Mahasiswa22 mahasiswa, Node22 right) {
+        this.mahasiswa = mahasiswa;
+        this.left = left;
+        this.right = right;
+    }
+}
